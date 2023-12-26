0a638f0b14b323bec837f3f402b6323e6567b399
Adiking117 <www.aditya117@gmail.com>
2023-12-26 14:37:52 +0530

    database connection

---
+++ backend/src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+import {DB_NAME} from "../constants.js"
+
+const connectDB = async() =>{
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        // console.log(connectionInstance);
+        console.log(`MongoDB connected at HOST : ${connectionInstance.connection.host}`)
+    } catch (error) {
+        console.log("MongoDb error : ",error)
+        process.exit(1);
+    }
+}
+
+export default connectDB;
