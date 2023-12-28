e71539d9640b24188e92a3850eefcfb1fda17da4
Adiking117 <www.aditya117@gmail.com>
2023-12-28 13:08:43 +0530

    added authentication for blog post actions

--- backend/src/controllers/blog.controller.js
+++ backend/src/controllers/blog.controller.js
@@ -1,147 +1,147 @@
-import {Blog} from "../models/blog.models.js"
-import { asyncHandler } from "../utils/asyncHandler.js"
-import { ApiError } from "../utils/ApiError.js"
-import { ApiResponse } from "../utils/ApiResponse.js"
-import { uploadOnCloudinary } from "../utils/cloudinary.js"
-import { User } from "../models/user.models.js"
+// import {Blog} from "../models/blog.models.js"
+// import { asyncHandler } from "../utils/asyncHandler.js"
+// import { ApiError } from "../utils/ApiError.js"
+// import { ApiResponse } from "../utils/ApiResponse.js"
+// import { uploadOnCloudinary } from "../utils/cloudinary.js"
+// import { User } from "../models/user.models.js"
 
-const getAllBlog = asyncHandler(async(req,res)=>{
-    const blog = await Blog.find();
-    if(blog.length === 0){
-        throw new ApiError(404,"Blogs not found")
-    }
-    return res
-    .status(200)
-    .json(
-        new ApiResponse(200,blog,"Blogs fetched successfully")
-    )
-})
+// const getAllBlog = asyncHandler(async(req,res)=>{
+//     const blog = await Blog.find();
+//     if(blog.length === 0){
+//         throw new ApiError(404,"Blogs not found")
+//     }
+//     return res
+//     .status(200)
+//     .json(
+//         new ApiResponse(200,blog,"Blogs fetched successfully")
+//     )
+// })
 
-const addBlog = asyncHandler(async(req,res)=>{
-    // console.log(req.body)
-    // console.log(req.files)
+// const addBlog = asyncHandler(async(req,res)=>{
+//     // console.log(req.body)
+//     // console.log(req.files)
 
-    const { title,description,postname,username } = req.body;
-    if(!(title && description && postname && username)){
-        throw new ApiError(401,"Please provide details")
-    }
+//     const { title,description,postname,username } = req.body;
+//     if(!(title && description && postname && username)){
+//         throw new ApiError(401,"Please provide details")
+//     }
 
-    const user = await User.findOne({username})
+//     const user = await User.findOne({username})
 
-    const imageLocalPath = req.files?.image[0]?.path
-    // console.log(imageLocalPath)
-    if(!imageLocalPath){
-        throw new ApiError(400,"Image path not found")
-    }
+//     const imageLocalPath = req.files?.image[0]?.path
+//     // console.log(imageLocalPath)
+//     if(!imageLocalPath){
+//         throw new ApiError(400,"Image path not found")
+//     }
 
-    const image = await uploadOnCloudinary(imageLocalPath)
-    if(!image){
-        throw new ApiError(400,"Image required")
-    }
+//     const image = await uploadOnCloudinary(imageLocalPath)
+//     if(!image){
+//         throw new ApiError(400,"Image required")
+//     }
 
-    const blog = await Blog.create({
-        postname,
-        title,
-        description,
-        image: image.url,
-        user:user._id
-    })
+//     const blog = await Blog.create({
+//         postname,
+//         title,
+//         description,
+//         image: image.url,
+//         user:user._id
+//     })
 
-    const createdBlog = Blog.findById(blog._id)
+//     const createdBlog = Blog.findById(blog._id)
 
-    if(!createdBlog){
-        throw new ApiError(500,"Something went wrong while uploading")
-    }
+//     if(!createdBlog){
+//         throw new ApiError(500,"Something went wrong while uploading")
+//     }
 
-    await User.findByIdAndUpdate(
-        user._id,
-        { $push: { blogs: blog._id } },
-        { new: true }
-    );
+//     await User.findByIdAndUpdate(
+//         user._id,
+//         { $push: { blogs: blog._id } },
+//         { new: true }
+//     );
 
-    return res
-    .status(200)
-    .json(
-        new ApiResponse(200,createdBlog,"Blog Created Successfully")
-    )
+//     return res
+//     .status(200)
+//     .json(
+//         new ApiResponse(200,createdBlog,"Blog Created Successfully")
+//     )
     
-})
+// })
 
-const updateBlog = asyncHandler(async(req,res)=>{
-    const { title,description } = req.body
-    if(!(title || description)){
-        throw new ApiError(401,"Please provide title to be updated")
-    }
+// const updateBlog = asyncHandler(async(req,res)=>{
+//     const { title,description } = req.body
+//     if(!(title || description)){
+//         throw new ApiError(401,"Please provide title to be updated")
+//     }
 
-    const updatedblog = await Blog.findOneAndUpdate(
-        {title,description},
-        {
-            $set:{
-                title:title,
-                description:description
-            }
-        },
-        {
-            new:true
-        }
-    )
-    console.log(updatedblog)
+//     const updatedblog = await Blog.findOneAndUpdate(
+//         {title,description},
+//         {
+//             $set:{
+//                 title:title,
+//                 description:description
+//             }
+//         },
+//         {
+//             new:true
+//         }
+//     )
+//     console.log(updatedblog)
 
-    return res
-    .status(200)
-    .json(
-        new ApiResponse(200,updatedblog,"Blog updated successfully")
-    )
-})
+//     return res
+//     .status(200)
+//     .json(
+//         new ApiResponse(200,updatedblog,"Blog updated successfully")
+//     )
+// })
 
-const likeBlog = asyncHandler(async(req,res)=>{
-    const {postname,username} = req.body
-    if(!(postname && username)){
-        throw new ApiError(400,"Please provide details")
-    }
+// const likeBlog = asyncHandler(async(req,res)=>{
+//     const {postname,username} = req.body
+//     if(!(postname && username)){
+//         throw new ApiError(400,"Please provide details")
+//     }
 
-    const user = await User.findOne({username})
-    const post = await Blog.findOne({postname})
+//     const user = await User.findOne({username})
+//     const post = await Blog.findOne({postname})
 
-    if(!(post && user)){
-        throw new ApiError(400,"Blog not dound")
-    }
+//     if(!(post && user)){
+//         throw new ApiError(400,"Blog not dound")
+//     }
 
-    await post.addLike(user.username)
+//     await post.addLike(user.username)
 
-    return res
-    .status(200)
-    .json(
-        new ApiResponse(200,post.likes.length,"Liked the post")
-    )
-})
+//     return res
+//     .status(200)
+//     .json(
+//         new ApiResponse(200,post.likes.length,"Liked the post")
+//     )
+// })
 
-const dislikeBlog = asyncHandler(async(req,res)=>{
-    const {postname,username} = req.body
-    if(!(postname && username)){
-        throw new ApiError(400,"Please provide details")
-    }
+// const dislikeBlog = asyncHandler(async(req,res)=>{
+//     const {postname,username} = req.body
+//     if(!(postname && username)){
+//         throw new ApiError(400,"Please provide details")
+//     }
 
-    const user = await User.findOne({username})
-    const post = await Blog.findOne({postname})
+//     const user = await User.findOne({username})
+//     const post = await Blog.findOne({postname})
 
-    if(!(post && user)){
-        throw new ApiError(400,"Blog not dound")
-    }
+//     if(!(post && user)){
+//         throw new ApiError(400,"Blog not dound")
+//     }
 
-    await post.removeLike(user.username)
+//     await post.removeLike(user.username)
 
-    return res
-    .status(200)
-    .json(
-        new ApiResponse(200,post.likes.length,"Disliked the post")
-    )
-})
+//     return res
+//     .status(200)
+//     .json(
+//         new ApiResponse(200,post.likes.length,"Disliked the post")
+//     )
+// })
 
-export{
-    getAllBlog,
-    addBlog,
-    updateBlog,
-    likeBlog,
-    dislikeBlog
-}
+// export{
+//     getAllBlog,
+//     addBlog,
+//     updateBlog,
+//     likeBlog,
+//     dislikeBlog
+// }
--- backend/src/routes/blog.routes.js
+++ backend/src/routes/blog.routes.js
@@ -1,22 +1,24 @@
 import express from "express"
-import { getAllBlog,addBlog, updateBlog, likeBlog, dislikeBlog } from "../controllers/blog.controller.js"
+import { getAllBlog,addBlog, updateBlog, likeBlog, dislikeBlog } from "../controllers/user.controllers.js"
 import { upload } from "../middlewares/multer.middlewares.js"
+import {verifyJWT} from "../middlewares/auth.middlewares.js"
 
 const blogRouter = express.Router()
 
 blogRouter.route("/").get(getAllBlog)
 blogRouter.route("/add").post(
+    verifyJWT,
     upload.fields([
         {
             name:"image",
-            maxCount: 1
+            maxCount: 5
         }
     ]),
     addBlog
 )
-blogRouter.route("/update-detail").post(updateBlog)
-blogRouter.route("/like-blog").post(likeBlog)
-blogRouter.route("/dislike-blog").post(dislikeBlog)
+blogRouter.route("/update-detail").post(verifyJWT,updateBlog)
+blogRouter.route("/like-blog").post(verifyJWT,likeBlog)
+blogRouter.route("/dislike-blog").post(verifyJWT,dislikeBlog)
 
 
 
